7dd6e6659c2e70bbacfc07f6ab38b3c4cd2eae75
imhere4thehorse <kaliechang10847@gmail.com>
2024-05-06 11:27:41 -0600

    everything except gitignore

---
+++ Github_Project/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void helloWorld() {
+		System.out.println("Hello World from Kalie");
+		}
+	public static void main(String[] args) {
+		helloWorld();
+		}
+}
